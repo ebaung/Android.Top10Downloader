44c660523edde24ee8f49dd45e49b921299ef409
Eugene Baung <ebaung@gmail.com>
2016-01-31 08:41:07 -0500

    Initial commit

---
+++ app/src/main/java/co/baung/top10downloader/Application.java
@@ -0,0 +1,41 @@
+package co.baung.top10downloader;
+
+/**
+ * Created by admin on 1/31/2016.
+ */
+public class Application {
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" +
+                "Artist: " + getArtist()+ "\n" +
+                "Release Date: " + getReleaseDate() + "\n";
+    }
+}
